edb96ef532a2eaeae8c46f0a85cc09e7f7117101
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-08-07 18:48:06 +0600

    task.js

---
+++ task6.js
@@ -0,0 +1,7 @@
+const students = [
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 }
+]
+
+console.log(students[0].name + " scored " + students[0].marks)
+console.log(students[0].name + " scored " + students[1].marks)
