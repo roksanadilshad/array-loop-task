2c67408ac05c2eaf8ee2754865460c74a4c9a6c9
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-08-07 18:16:50 +0600

    task4.js

--- task3.js
+++ task3.js
@@ -2,7 +2,7 @@ var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
 var result = "";
 
 for(var number of numbers){
-    result += number;
+    result += number; //for...of loop er khetre of er age ja tahkbe oita notun variable er sathe jog korta lagbe.
     
 }
 console.log(result);
---
+++ task4.js
@@ -0,0 +1,6 @@
+const statement = 'I am a hard working person';
+
+const reversed = statement.split(" ").reverse().join(" ");
+
+
+console.log(reversed);
